19256fcc3caebe851ac78bdba63914f851ee9d2b
dcaayushd <aayushdc123@yahoo.com>
2025-03-17 14:06:44 +0545

    ASP.NET CORE firstprogram Added

---
+++ ASP.NET CORE/firstprogram/Program.cs
@@ -0,0 +1,14 @@
+namespace firstprogram;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
