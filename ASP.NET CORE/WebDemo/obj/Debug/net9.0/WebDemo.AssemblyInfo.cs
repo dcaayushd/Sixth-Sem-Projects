74eeb6584d8f71a0190d0dbdb56a6c8589e808d9
dcaayushd <aayushdc123@yahoo.com>
2025-03-21 22:13:01 +0545

    ASP.NET CORE Web App(MVC)  Added

--- ASP.NET CORE/WebDemo/obj/Debug/net9.0/WebDemo.AssemblyInfo.cs
+++ ASP.NET CORE/WebDemo/obj/Debug/net9.0/WebDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webdemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6512e027076817497839d3e6ac74bc7edc43e234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175d9e2e042d3866d9086e6714fba4b293229eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webdemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webdemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
