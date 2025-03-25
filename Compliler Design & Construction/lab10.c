0740080861e24de7116417cdae2a6461ccbd476b
dcaayushd <aayushdc123@yahoo.com>
2025-03-25 20:54:57 +0545

    Final Code Generation Updated

--- Compliler Design & Construction/lab10.c
+++ Compliler Design & Construction/lab10.c
@@ -1,11 +1,12 @@
 // Program: Final Code Generation
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 char op[2], arg1[5], arg2[5], result[5];
 
-int main() {
+int main()
+{
     FILE *fp1, *fp2;
 
     // Open input and output files
@@ -13,36 +14,43 @@ int main() {
     fp2 = fopen("output.txt", "w");
 
     // Check if files opened successfully
-    if (fp1 == NULL || fp2 == NULL) {
+    if (fp1 == NULL || fp2 == NULL)
+    {
         printf("Error opening file\n");
         return 1;
     }
 
     // Read the operations from input file and generate the corresponding final code
-    while (!feof(fp1)) {
+    while (!feof(fp1))
+    {
         fscanf(fp1, "%s%s%s%s", op, arg1, arg2, result);
 
-        if (strcmp(op, "+") == 0) {
+        if (strcmp(op, "+") == 0)
+        {
             fprintf(fp2, "\n MOV R0, %s", arg1);
             fprintf(fp2, "\n ADD R0, %s", arg2);
             fprintf(fp2, "\n MOV %s, R0", result);
         }
-        if (strcmp(op, "*") == 0) {
+        if (strcmp(op, "*") == 0)
+        {
             fprintf(fp2, "\n MOV R0, %s", arg1);
             fprintf(fp2, "\n MUL R0, %s", arg2);
             fprintf(fp2, "\n MOV %s, R0", result);
         }
-        if (strcmp(op, "-") == 0) {
+        if (strcmp(op, "-") == 0)
+        {
             fprintf(fp2, "\n MOV R0, %s", arg1);
             fprintf(fp2, "\n SUB R0, %s", arg2);
             fprintf(fp2, "\n MOV %s, R0", result);
         }
-        if (strcmp(op, "/") == 0) {
+        if (strcmp(op, "/") == 0)
+        {
             fprintf(fp2, "\n MOV R0, %s", arg1);
             fprintf(fp2, "\n DIV R0, %s", arg2);
             fprintf(fp2, "\n MOV %s, R0", result);
         }
-        if (strcmp(op, "=") == 0) {
+        if (strcmp(op, "=") == 0)
+        {
             fprintf(fp2, "\n MOV R0, %s", arg1);
             fprintf(fp2, "\n MOV %s, R0", result);
         }
