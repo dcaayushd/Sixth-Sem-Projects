30f3fbcd254bf7a0ae2af5f82c51264efca19f5c
dcaayushd <aayushdc123@yahoo.com>
2025-03-29 20:23:27 +0545

    Updated

--- Compliler Design & Construction/lab7.c
+++ Compliler Design & Construction/lab7.c
@@ -11,15 +11,15 @@
 
 char s[20], stack[20];
 
-int main() {
+int main()
+{
     // LL(1) Parsing Table (Grammar Production Rules)
     char m[5][6][4] = {
         {"tb", " ", " ", "tb", " ", " "},
         {" ", "+tb", " ", " ", "n", "n"},
         {"fc", " ", " ", "fc", " ", " "},
         {" ", "n", "*fc", "n", "n", "n"},
-        {"i", " ", " ", "(e)", " ", " "}
-    };
+        {"i", " ", " ", "(e)", " ", " "}};
 
     // Size table to track production lengths
     int size[5][6] = {
@@ -27,8 +27,7 @@ int main() {
         {0, 3, 0, 0, 1, 1},
         {2, 0, 0, 2, 0, 0},
         {0, 1, 3, 0, 1, 1},
-        {1, 0, 0, 3, 0, 0}
-    };
+        {1, 0, 0, 3, 0, 0}};
 
     int i, j, k, n, str1, str2;
 
@@ -47,44 +46,79 @@ int main() {
     printf("\nStack     Input\n");
     printf("__________________\n");
 
-    while ((stack[i] != '$') && (s[j] != '$')) {
-        if (stack[i] == s[j]) {
+    while ((stack[i] != '$') && (s[j] != '$'))
+    {
+        if (stack[i] == s[j])
+        {
             i--;
             j++;
         }
 
         // Map stack top to parsing table row index
-        switch (stack[i]) {
-            case 'e': str1 = 0; break;
-            case 'b': str1 = 1; break;
-            case 't': str1 = 2; break;
-            case 'c': str1 = 3; break;
-            case 'f': str1 = 4; break;
-            default: str1 = -1;
+        switch (stack[i])
+        {
+        case 'e':
+            str1 = 0;
+            break;
+        case 'b':
+            str1 = 1;
+            break;
+        case 't':
+            str1 = 2;
+            break;
+        case 'c':
+            str1 = 3;
+            break;
+        case 'f':
+            str1 = 4;
+            break;
+        default:
+            str1 = -1;
         }
 
         // Map input symbol to parsing table column index
-        switch (s[j]) {
-            case 'i': str2 = 0; break;
-            case '+': str2 = 1; break;
-            case '*': str2 = 2; break;
-            case '(': str2 = 3; break;
-            case ')': str2 = 4; break;
-            case '$': str2 = 5; break;
-            default: str2 = -1;
+        switch (s[j])
+        {
+        case 'i':
+            str2 = 0;
+            break;
+        case '+':
+            str2 = 1;
+            break;
+        case '*':
+            str2 = 2;
+            break;
+        case '(':
+            str2 = 3;
+            break;
+        case ')':
+            str2 = 4;
+            break;
+        case '$':
+            str2 = 5;
+            break;
+        default:
+            str2 = -1;
         }
 
-        if (str1 == -1 || str2 == -1 || m[str1][str2][0] == '\0') {
+        if (str1 == -1 || str2 == -1 || m[str1][str2][0] == '\0')
+        {
             printf("\nERROR: Invalid input string.\n");
             exit(0);
         }
 
-        if (m[str1][str2][0] == 'n') {
+        if (m[str1][str2][0] == 'n')
+        {
             i--; // Pop stack
-        } else if (m[str1][str2][0] == 'i') {
+        }
+        else if (m[str1][str2][0] == 'i')
+        {
             stack[i] = 'i'; // Replace stack top
-        } else {
-            for (k = size[str1][str2] - 1; k >= 0; k--) {
+        }
+        else
+        {
+            for (k = size[str1][str2] - 1; k >= 0; k--)
+            {
                 stack[i] = m[str1][str2][k];
                 i++;
             }
