35e07367264db9825ad56c367067b993b31af8ea
dcaayushd <aayushdc123@yahoo.com>
2025-03-30 16:14:22 +0545

    Updated

--- Compliler Design & Construction/lab6.c
+++ Compliler Design & Construction/lab6.c
@@ -16,11 +16,12 @@
 int n, i = 0, j = 0;
 char a[10][10], Result[10], subResult[20];
 
-void follow(char* Result, char c);
-void first(char* Result, char c);
+void follow(char *Result, char c);
+void first(char *Result, char c);
 void addToResultSet(char[], char);
 
-int main() {
+int main()
+{
     int choice;
     char c;
 
@@ -31,7 +32,8 @@ int main() {
     for (i = 0; i < n; i++)
         scanf("%s", a[i]);
 
-    do {
+    do
+    {
         printf("Find FOLLOW of --> ");
         scanf(" %c", &c);
 
@@ -50,7 +52,8 @@ int main() {
     return 0;
 }
 
-void follow(char* Result, char c) {
+void follow(char *Result, char c)
+{
     int k;
     subResult[0] = '\0';
     Result[0] = '\0';
@@ -59,9 +62,12 @@ void follow(char* Result, char c) {
     if (a[0][0] == c)
         addToResultSet(Result, '$');
 
-    for (i = 0; i < n; i++) {
-        for (j = 2; j < strlen(a[i]); j++) {
-            if (a[i][j] == c) {
+    for (i = 0; i < n; i++)
+    {
+        for (j = 2; j < strlen(a[i]); j++)
+        {
+            if (a[i][j] == c)
+            {
                 // If B → αAβ, then FIRST(β) ⊆ FOLLOW(A)
                 if (a[i][j + 1] != '\0')
                     first(subResult, a[i][j + 1]);
@@ -77,15 +83,18 @@ void follow(char* Result, char c) {
     }
 }
 
-void first(char* R, char c) {
+void first(char *R, char c)
+{
     int k, m;
 
     // If it's a terminal, add it to the FIRST set
     if (!isupper(c) && c != '#')
         addToResultSet(R, c);
 
-    for (k = 0; k < n; k++) {
-        if (a[k][0] == c) {
+    for (k = 0; k < n; k++)
+    {
+        if (a[k][0] == c)
+        {
             if (a[k][2] == '#' && c != a[i][0])
                 follow(R, a[i][0]);
             else if (!isupper(a[k][2]) && a[k][2] != '#')
@@ -99,7 +108,8 @@ void first(char* R, char c) {
     }
 }
 
-void addToResultSet(char Result[], char val) {
+void addToResultSet(char Result[], char val)
+{
     int k;
     for (k = 0; Result[k] != '\0'; k++)
         if (Result[k] == val)
